12ad78b3f587431826d3663fd9ead2e827d5df2a
billybrown237 <kano.billybrown@gmail.com>
2024-05-29 20:49:51 +0100

    chore:removed dist folder from cache

--- dist/index.mjs
+++
@@ -1,9 +0,0 @@
-// src/ProgressSteps/ProgressButtons.tsx
-import React from "react";
-import { View } from "react-native";
-function ProgressButtons({ renderPreviousButton, renderNextButton }) {
-  return /* @__PURE__ */ React.createElement(View, { style: { flexDirection: "row", marginTop: 90 } }, /* @__PURE__ */ React.createElement(View, { style: { position: "absolute", left: 60, bottom: 40 } }, renderPreviousButton()), /* @__PURE__ */ React.createElement(View, { style: { position: "absolute", right: 60, bottom: 40 } }, renderNextButton()));
-}
-export {
-  ProgressButtons
-};
